9e0a745362690fb31ce7fe9e961216d130dc199c
Sadid <66120003+sadid369@users.noreply.github.com>
2024-06-30 02:33:22 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('about',function ()  {
+    return "<h1>About Page</h1>";
+})->name('a');
+
+Route::get('contact', function () {
+    return "<h1>Contact</h1>";
+});
+
+Route::get('users/{id}', function ($id) {
+    return $id;
+})->name('c');
+
+Route::get('home', function () {
+    return "<a href='".route('c',1)."'>About</a>";
+});
+
+
+
+Route::group(["prefix"=>"customer"],function () {
+    Route::get('/', function () {
+        return "<h1>Customer</h1>";
+    });
+    Route::get('create', function () {
+        return "<h1>Customer Create</h1>";
+    });
+    Route::get('show', function () {
+        return "<h1>Customer Show</h1>";
+    });
+});
+
+
+Route::fallback(function () {
+    return "<h1>Route Does not Exist </h1>";
+});
